b0d8414d2d2b2517225e7f44c6fe8e135790bdef
Will S <fitnesswill13@gmail.com>
2022-09-30 16:46:25 -0700

    completed html outline and stylesheet

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+//establish variables which is everything we need to keep track
+
+//start page with button that says take quiz
+
+//button event listener for click, then changes the centered text to question 1 also starting the setinterval timer function
+//timer should be 20
+//two functions set in this one starts timer other changes quiz to questions
+
+//questions should have 4 separate buttons each one submitting answer use element.matches to determine correct answer
+// if wrong take time off 
+//
+
+
+/*
+
+
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```*/
